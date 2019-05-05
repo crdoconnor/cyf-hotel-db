1b320bfc79cfca1130120961f161be269f3c53b6
Colm O'Connor <colm.oconnor.github@gmail.com>
2019-05-05 16:05:51 +0100

    Removed sqlite code.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const SERVER_PORT = process.env.PORT || 8080;
 
-const resetDataBase = require('./utils/db-tools');
-resetDataBase();
-
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyparser = require("body-parser");
